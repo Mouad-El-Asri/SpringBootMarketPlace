0e4fcffa5d298bf07fc98ee022ab8174fe18eaca
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-02-07 14:07:02 +0100

    Initial commit: Set up basic Spring Boot project structure.

---
+++ src/main/java/com/SpringBootStarters/FreshSpringBoot/FreshSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.SpringBootStarters.FreshSpringBoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreshSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreshSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SpringBootStarters/FreshSpringBoot/FreshSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SpringBootStarters.FreshSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreshSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
