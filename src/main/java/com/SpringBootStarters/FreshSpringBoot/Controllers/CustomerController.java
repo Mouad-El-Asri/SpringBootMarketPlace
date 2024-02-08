e2eb1f2cea684e04103c459671562f74c0b5f27b
Mouad-El-Asri <elasri.mouad.2002@gmail.com>
2024-02-08 17:15:50 +0100

    Integrate Spring Boot with OpenAPI and Swagger for API documentation

--- src/main/java/com/SpringBootStarters/FreshSpringBoot/Controllers/CustomerController.java
+++ src/main/java/com/SpringBootStarters/FreshSpringBoot/Controllers/CustomerController.java
@@ -2,19 +2,22 @@ package com.SpringBootStarters.FreshSpringBoot.Controllers;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.SpringBootStarters.FreshSpringBoot.Entities.Customer;
 import com.SpringBootStarters.FreshSpringBoot.Services.CustomerService;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
 
 @RestController
-@RequestMapping(path = "api/v1/customer")
+@RequestMapping(path = "api/v1/customers")
+@Tag(name = "Customer", description = "The customer API")
 public class CustomerController {
 	private final CustomerService customerService;
 
@@ -27,14 +30,15 @@ public class CustomerController {
 	 *
 	 * @return the list of customers
 	 */
-	@GetMapping("/customers")
+	@GetMapping("/")
+	@Operation(summary = "Get all customers", description = "Get all customers data from the database")
 	public List<Customer> getCustomers() {
 		return this.customerService.getCustomers();
 	}
 
 	@PostMapping("/create")
+	@Operation(summary = "Create a new customer", description = "Create a new customer in the database")
 	public Customer postMethodName(@RequestBody Customer customer) {
 		return customer;
 	}
-	
 }
